68464ba5c58eb46352e6f226c3be01ca22b55478
Edun Omatseye <edunomatseye@yahoo.com>
2023-10-24 13:00:43 +0100

    chore: added JobsRepository to handle Sequelize database operations

---
+++ src/repository/jobs.js
@@ -0,0 +1,137 @@
+const { Contract, Job, Profile, sequelize } = require("../model");
+const { Op } = require("sequelize");
+
+class JobsRepository {
+  createBaseQuery(start, end) {
+    return {
+      where: {
+        paid: 1,
+        createdAt: { [Op.gte]: start },
+        paymentDate: { [Op.lte]: end },
+      },
+      include: [
+        {
+          model: Contract,
+          attributes: [],
+          include: [
+            {
+              model: Profile,
+              as: "Client",
+              attributes: [],
+            },
+          ],
+        },
+      ],
+    };
+  }
+
+  async findBestPayingClients(start, end, limit = 2) {
+    const query = this.createBaseQuery(start, end);
+    query.group = ["Contract.Client.id"];
+    query.attributes = [
+      [sequelize.col("Contract.Client.id"), "id"],
+      [sequelize.col("Contract.Client.firstName"), "firstName"],
+      [sequelize.col("Contract.Client.lastName"), "lastName"],
+      [sequelize.fn("SUM", sequelize.col("price")), "paid"],
+    ];
+    query.order = [[sequelize.col("paid"), "DESC"]];
+    query.limit = limit;
+    query.raw = true;
+
+    return Job.findAll(query);
+  }
+
+  async findBestProfession(start, end) {
+    const query = this.createBaseQuery(start, end);
+    query.group = ["Contract.Contractor.profession"];
+    query.attributes = [
+      [sequelize.col("Contract.Contractor.profession"), "profession"],
+      [sequelize.fn("SUM", sequelize.col("price")), "total"],
+    ];
+    query.order = [[sequelize.col("total"), "DESC"]];
+    query.raw = true;
+
+    return Job.findOne(query);
+  }
+
+  async getTotalJobsCost(clientId) {
+    const query = {
+      where: {
+        paid: { [Op.is]: null },
+      },
+      include: [
+        {
+          model: Contract,
+          required: true,
+          attributes: [],
+          where: {
+            ClientId: clientId,
+          },
+          include: [
+            {
+              model: Profile,
+              as: "Client",
+              required: true,
+              attributes: [],
+            },
+          ],
+        },
+      ],
+      attributes: [[sequelize.fn("SUM", sequelize.col("price")), "total"]],
+      raw: true,
+    };
+
+    return Job.findOne(query);
+  }
+
+  async getUnpaidJobs(profileId) {
+    const query = {
+      where: {
+        [Op.or]: [
+          { "$Contract.ContractorId$": profileId },
+          { "$Contract.ClientId$": profileId },
+        ],
+        "$Contract.status$": { [Op.not]: "terminated" },
+      },
+      include: [
+        {
+          model: Contract,
+          required: true,
+        },
+      ],
+    };
+
+    return Job.findAll(query);
+  }
+
+  async getJob(id) {
+    const query = {
+      where: {
+        id,
+      },
+      include: [
+        {
+          model: Contract,
+          required: true,
+          attributes: ["ContractorId", "ClientId"],
+          include: [
+            {
+              model: Profile,
+              as: "Client",
+              required: true,
+            },
+            {
+              model: Profile,
+              as: "Contractor",
+              required: true,
+            },
+          ],
+        },
+      ],
+    };
+
+    return Job.findOne(query);
+  }
+}
+
+module.exports = { JobsRepository };
