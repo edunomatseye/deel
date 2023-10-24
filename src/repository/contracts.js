4cd09a17a6dcf0f14b5a6590a96f91163ee1df08
Edun Omatseye <edunomatseye@yahoo.com>
2023-10-24 12:46:15 +0100

    chore: add ContractRepo class with listNonTerminatedContracts & findContractsOwnedBy methods

---
+++ src/repository/contracts.js
@@ -0,0 +1,27 @@
+const { Contract } = require("../model");
+
+class ContractsRepository {
+  async listNonTerminatedContracts(profileId) {
+    const query = {
+      where: {
+        status: { [Op.not]: "terminated" },
+        [Op.or]: [{ ContractorId: profileId }, { ClientId: profileId }],
+      },
+    };
+
+    return Contract.findAll(query);
+  }
+
+  async findContractOwnedBy(id, profileId) {
+    const query = {
+      where: {
+        id,
+        [Op.or]: [{ ContractorId: profileId }, { ClientId: profileId }],
+      },
+    };
+
+    return Contract.findOne(query);
+  }
+}
+
+module.exports = { ContractsRepository };
