0ccf5962a11f54ca5695a0b0dfac7fe49315ff16
Edun Omatseye <edunomatseye@yahoo.com>
2023-10-24 12:23:08 +0100

    test: added jest test to getProfile.js and ensured it only returned owners contract

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    testEnvironment: 'node',
+    moduleFileExtensions: ['js', 'json'],
+    transform: {
+      '^.+\\.(js|jsx)$': './node_modules/babel-jest',
+    },
+    transformIgnorePatterns: ['./node_modules/'],
+    testMatch: ['**/__tests__/**/*.js', '**/?(*.)+(spec|test).js'],
+    coverageThreshold: {
+      global: {
+        branches: 80,
+        functions: 80,
+        lines: 80,
+        statements: 80,
+      },
+    },
+    coverageReporters: ['json', 'lcov'],
+  };
---
+++ test/middleware/getProfile.test.js
@@ -0,0 +1,29 @@
+const request = require("supertest");
+const app = require("../../src/app");
+const { Profile } = app.get("models");
+
+describe("Middleware getProfile", () => {
+  let server;
+
+  beforeAll(() => {
+    server = app.listen();
+  });
+
+  afterAll((done) => {
+    server.close(done);
+  });
+
+  it("should return 401 if profile is not set in the header", async () => {
+    jest.spyOn(Profile, "findOne").mockResolvedValue(null);
+    const response = await request(server).get("/contracts/1");
+
+    expect(response.status).toBe(401);
+  });
+
+  it("should return 401 if profile is not in the database", async () => {
+    jest.spyOn(Profile, "findOne").mockResolvedValue(null);
+    const response = await request(server).get("/contracts/1").set("profile_id", "1");
+
+    expect(response.status).toBe(401);
+  });
+});
