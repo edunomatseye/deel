2275ac69519c4c0289e3212f3fdfb1c7d8392e45
Edun Omatseye <edunomatseye@yahoo.com>
2023-11-02 08:45:24 +0100

    test(controller) added test for balance controller

---
+++ test/controller/balance.test.js
@@ -0,0 +1,49 @@
+const { makeClientDeposit } = require('../../src/controller/balance'); // Replace with your actual controller file
+const { DepositService } = require('../../src/services/deposit');
+jest.mock('../../src/services/deposit');
+
+describe('makeClientDeposit', () => {
+  let req, res, next;
+  let depositService;
+
+  beforeEach(() => {
+    req = { params: {}, body: {} };
+    res = {
+      status: jest.fn(() => res),
+      json: jest.fn(),
+    };
+    next = jest.fn();
+  });
+
+  it('should make a client deposit and return success response', async () => {
+    // Arrange
+    req.params.clientId = 1;
+    req.body.amount = 100;
+    DepositService.prototype.deposit.mockResolvedValue(200);
+
+    // Act
+    await makeClientDeposit(req, res, next);
+
+    // Assert
+    expect(DepositService.prototype.deposit).toHaveBeenCalledWith(1, 100);
+    expect(res.status).toHaveBeenCalledWith(200);
+    expect(res.json).toHaveBeenCalledWith({ message: 'Deposit was successfully - balance: 200' });
+    expect(next).not.toHaveBeenCalled();
+  });
+
+  it('should handle deposit service error and return an error response', async () => {
+    // Arrange
+    req.params.clientId = 1;
+    req.body.amount = 100;
+    DepositService.prototype.deposit.mockRejectedValue(new Error('Deposit failed'));
+
+    // Act
+    await makeClientDeposit(req, res, next);
+
+    // Assert
+    expect(DepositService.prototype.deposit).toHaveBeenCalledWith(1, 100);
+    expect(res.status).toHaveBeenCalledWith(401);
+    expect(res.json).toHaveBeenCalledWith({ error: 'Deposit failed' });
+    expect(next).toHaveBeenCalled();
+  });
+});
