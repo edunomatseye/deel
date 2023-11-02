2dcdcf90d95a45a8480771ee317e19438d50cef8
Edun Omatseye <edunomatseye@yahoo.com>
2023-11-02 18:58:25 +0100

    chore: added stress test, benchmark with concurrency - autocannon

---
+++ benchmarks/basic.js
@@ -0,0 +1,39 @@
+const autocannon = require("autocannon");
+require("dotenv").config();
+
+function startBench() {
+  const url = "http://localhost:" + (process.env.PORT || 3001);
+
+  const args = process.argv.slice(2);
+  const connections = args[0] || 1000;
+  const maxConnectionRequests = args[1] || 1000;
+
+  const instance = autocannon(
+    {
+      title: 'Deel concurrent connections',
+      url,
+      connections,
+      duration: 10,
+      maxConnectionRequests,
+      headers: {
+        "content-type": "application/json",
+        "profile_id": "3",
+      },
+      requests: [
+        {
+          method: "GET",
+          path: "/",
+        },
+      ],
+    },
+    finishedBench
+  );
+
+  autocannon.track(instance);
+
+  function finishedBench(err, res) {
+    console.log("Finished Bench", err, res);
+  }
+}
+
+startBench();
--- src/router.js
+++ src/router.js
@@ -12,5 +12,6 @@ router.get("/jobs/unpaid", getAllUnpaidJob);
 router.post("/jobs/:job_id/pay", makePaymentForJobDone);
 router.get("/contracts/:id", getContractsById);
 router.get("/contracts", getContractList);
+router.get("/", (req, res) => { res.json({ message: 'welcome to homepage!'})})
 
 module.exports = { appRouter: router };
