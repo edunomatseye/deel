509e204753f3e31fa1d08b1670800dc92572146c
Edun Omatseye <edunomatseye@yahoo.com>
2023-11-09 21:07:10 +0100

    chore: refactor getProfile to handle error properly

--- src/middleware/getProfile.js
+++ src/middleware/getProfile.js
@@ -1,17 +1,17 @@
 const getProfile = async (req, res, next) => {
   const { Profile } = req.app.get("models");
+
   try {
     const profile = await Profile.findOne({
       where: { id: req.get("profile_id") || 0 },
     });
-    if (!profile)
-      return res.status(401).json({ error: `Profile does not exist!` });
+
+    if (!profile) return res.status(404).json({ error: `Profile not found!` });
     req.profile = profile;
     next();
   } catch (err) {
-    if (!profile)
-      return res.status(401).json({ error: `Prodile does not exist!` });
-    next(err);
+    res.status(401).json({ error: `Profile does not exist!` });
+    throw err;
   }
 };
 module.exports = { getProfile };
