2b82375c02db2eb4b60d129da3a85f24b598586b
Edun Omatseye <edunomatseye@yahoo.com>
2023-11-16 19:41:49 +0100

    chore: added helmet security package

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const helmet = require("helmet");
+
 const { sequelize } = require("./model");
 const { getProfile } = require("./middleware/getProfile");
 const { errorHandler } = require("./middleware/errorHandler");
 const { appRouter } = require("./router");
 
 const app = express();
+app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
